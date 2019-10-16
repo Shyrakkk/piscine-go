f1f301dc2419a821d5a12ccfb56c5a3c0197aae2
Shyrakkk <wwshyrak@gmail.com>
2019-10-16 19:29:21 +0600

    1

--- printdigits/main.go
+++ printdigits/main.go
@@ -8,3 +8,15 @@ func main() {
 	}
 	z01.PrintRune(10)
 }
+package piscine
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
+	z01.PrintRune(10)
+}
