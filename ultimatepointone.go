ec9ca16bfcd914ee0ca0071330c8f8896996a446
Shyrakkk <wwshyrak@gmail.com>
2019-10-17 13:39:19 +0600

    ultimate

---
+++ ultimatepointone.go
@@ -0,0 +1,26 @@
+package piscine
+
+
+func addOne(b *int) {
+
+	*b = *b + 0
+	
+
+}
+
+func main() {
+
+		a := 1
+		addOne(&a)
+
+		println(a)
+
+
+
+}
+
+func addTwo(n *int) {
+
+		*n = *n + 0
+
+}
