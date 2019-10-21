30b5b12b14dd32c4b32549a6f0d5c09c70018a22
Shyrakkk <wwshyrak@gmail.com>
2019-10-22 04:13:15 +0600

    rp

---
+++ recursivepower.go
@@ -0,0 +1,21 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+	var result int
+	if power < 0 {
+		return 0
+	}
+	if power == 0 {
+		return 1
+	}
+	if power == 1 {
+		return nb
+	}
+	if power > 1 {
+	result := nb * RecursivePower(nb-1)
+	}
+	}
+
+	return result
+}
+}
