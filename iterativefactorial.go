d423a463b58b0258ea9ec783063585106682ee75
Shyrakkk <wwshyrak@gmail.com>
2019-10-21 20:13:55 +0600

    iteractive

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -7,11 +7,11 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
-	if nb > 0 && nb < 26 { 
+	if nb > 0 && nb < 26 {
 		result := 1
 		for a := 1; a <= nb; a++ {
 			result = a * result
-		} 	
+		}
 		return result
 	}
 }
