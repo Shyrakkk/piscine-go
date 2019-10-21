7476c895f9deed97a7ca3f476f80f39b000e5c21
Shyrakkk <wwshyrak@gmail.com>
2019-10-21 23:57:26 +0600

    recursivfactorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,7 +4,7 @@ func  RecursiveFactorial(nb int) int {
 	if nb >= -20 && nb <= 20 {
 		return nb * RecursiveFactorial(nb -1)
 		}
-	if nb==0 || nb == 1
+	if nb==0 || nb == 1 {
 		return 1
 	
 		} else {
