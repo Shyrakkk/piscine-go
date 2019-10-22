d676233db687cf1fdb038f3aa4407ec73f096922
Shyrakkk <wwshyrak@gmail.com>
2019-10-22 19:54:04 +0600

    fibon

--- fibonacci.go
+++ fibonacci.go
@@ -3,10 +3,11 @@ package piscine
 func Fibonacci(index int) int {
 
 	if index < 0 {
-		return index
+		return -1
+	} else if index == 0 {
+		return 1
 	} else {
 		return Fibonacci(index-1) + Fibonacci(index-2)
-
 	}
 
 }
