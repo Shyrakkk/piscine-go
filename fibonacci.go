4ebf14902826888c96cfb455f38522205248d8fd
Shyrakkk <wwshyrak@gmail.com>
2019-10-22 19:50:16 +0600

    fibon

--- fibonacci.go
+++ fibonacci.go
@@ -2,10 +2,11 @@ package piscine
 
 func Fibonacci(index int) int {
 	a := index - 1
+	b := index - 2
 	if index < 0 {
 		return -1
 	} else {
-		return Fibonacci(index-1) + Fibonacci(index-2)
+		return Fibonacci(a) + Fibonacci(b)
 
 	}
 
