ecce04d28ea756a14e1abe37aa55cdab6923fd34
Shyrakkk <wwshyrak@gmail.com>
2019-10-23 19:34:33 +0600

    nrune

--- nrune.go
+++ nrune.go
@@ -1,17 +1,11 @@
 package piscine
 
 func NRune(s string, n int) rune {
-
 	shsh := []rune(s)
 	for i, j := range shsh {
-		
-		if i == n-1
-		return j
-
+		if i == n-1 {
+			return j
+		}
 	}
-	
-
 	return 0
-
-
 }
