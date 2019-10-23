2706e539d796599f73057cdc6484dc949862130e
Shyrakkk <wwshyrak@gmail.com>
2019-10-23 17:35:35 +0600

    trimatoi

--- trimatoi.go
+++ trimatoi.go
@@ -7,8 +7,8 @@ func TrimAtoi(s string) int {
 	for _, i := range s {
 		if i >= '0' && i <= '9' {
 			c := int(i - 48)
-			a = X*10 + b
-		} else if i == '-' && X == 0 { //
+			a = a*10 + c
+		} else if i == '-' && a == 0 { //
 			b = -1
 		}
 	}
